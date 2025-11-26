36d86356c24c3a5c0963b5699547370a54ec8d74
Mike Mupararano <kudath@yahoo.co.uk>
2025-11-26 21:21:28 +0000

    Added code to payment_process views as follows: price data, unit amount, product information and the number of units to purchase.

--- payment/views.py
+++ payment/views.py
@@ -9,7 +9,7 @@ from orders.models import Order
 # Create the Stripe instance
 stripe.api_key = settings.STRIPE_SECRET_KEY
 stripe.api_version = settings.STRIPE_API_VERSION
-def payment_page(request):
+def payment_process(request):
     order_id = request.session.get('order_id')
     order = get_object_or_404(Order, id=order_id)
     if request.method == 'POST':
@@ -27,6 +27,20 @@ def payment_page(request):
             'cancel_url': cancel_url,
             'line_items': []
         }
+        # Add the order items to the Stripe checkout session
+        for item in order.items.all():
+            session_data['line_items'].append(
+                {
+                'price_data': {
+                    'unit_amount': int(item.price * Decimal('100')),
+                    'currency': 'gbp',
+                    'product_data': {
+                        'name': item.product.name,
+                    },
+                   
+                },
+                'quantity': item.quantity,
+            })
         # Create Stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
         # Redirect to Stripe payment form
