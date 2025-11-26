48ca04d9987e6dec273c9db0515e8aef1f0aa593
Mike Mupararano <kudath@yahoo.co.uk>
2025-11-26 20:29:03 +0000

    Wrote import statements in payemts/views.py for various modules.

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+frm decimal import Decimal
+from django.shortcuts import get_object_or_404, redirect, render
+import stripe
+from django.conf import settings
+from django.urls import reverse
+from orders.models import Order
 
 # Create your views here.
