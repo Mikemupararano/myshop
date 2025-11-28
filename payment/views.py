1d3ae09e3fe7a79ca7f9745e354c135a4134ce70
Mike Mupararano <kudath@yahoo.co.uk>
2025-11-28 21:57:14 +0000

    Corrected a typo in line 18 of payment/views. It was written as "succeed" instead of "success".

--- payment/views.py
+++ payment/views.py
@@ -9,49 +9,50 @@ from orders.models import Order
 # Create the Stripe instance
 stripe.api_key = settings.STRIPE_SECRET_KEY
 stripe.api_version = settings.STRIPE_API_VERSION
+
+
 def payment_process(request):
-    order_id = request.session.get('order_id')
+    order_id = request.session.get("order_id")
     order = get_object_or_404(Order, id=order_id)
-    if request.method == 'POST':
-        suceed_url = request.build_absolute_uri(
-            reverse('payment:completed')
-            )
-        cancel_url = request.build_absolute_uri(
-            reverse('payment:cancelled')
-            )
+    if request.method == "POST":
+        success_url = request.build_absolute_uri(reverse("payment:completed"))
+        cancel_url = request.build_absolute_uri(reverse("payment:cancelled"))
         # Create a new Stripe checkout data
         session_data = {
-            'mode': 'payment',
-            'client_reference_id': order.id,
-            'success_url': success_url,
-            'cancel_url': cancel_url,
-            'line_items': []
+            "mode": "payment",
+            "client_reference_id": order.id,
+            "success_url": success_url,
+            "cancel_url": cancel_url,
+            "line_items": [],
         }
         # Add the order items to the Stripe checkout session
         for item in order.items.all():
-            session_data['line_items'].append(
+            session_data["line_items"].append(
                 {
-                'price_data': {
-                    'unit_amount': int(item.price * Decimal('100')),
-                    'currency': 'gbp',
-                    'product_data': {
-                        'name': item.product.name,
+                    "price_data": {
+                        "unit_amount": int(item.price * Decimal("100")),
+                        "currency": "gbp",
+                        "product_data": {
+                            "name": item.product.name,
+                        },
                     },
-                   
-                },
-                'quantity': item.quantity,
-            })
+                    "quantity": item.quantity,
+                }
+            )
         # Create Stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
         # Redirect to Stripe payment form
         return redirect(session.url, code=303)
-    else: 
-        return render (request, 'payment/process.html', locals())
-    
+    else:
+        return render(request, "payment/process.html", locals())
+
     # Creating a view to handle successful payments
+
+
 def payment_completed(request):
-    return render(request, 'payment/completed.html')
+    return render(request, "payment/completed.html")
+
 
 # Creating a view to handle cancelled payments
 def payment_cancelled(request):
-    return render(request, 'payment/cancelled.html')
+    return render(request, "payment/cancelled.html")
