11c4713ee8c18c29ce0bedcf57d3b0505b953b27
Mike Mupararano <kudath@yahoo.co.uk>
2025-12-06 20:44:35 +0000

    Had to undo the code in apps.py because I put the Orders model code in there by mistake.

--- orders/apps.py
+++ orders/apps.py
@@ -2,8 +2,5 @@ from django.apps import AppConfig
 
 
 class OrdersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'orders'
-
-class Order(models.Model):
-    stripe_id = models.name = models.CharField(max_length=250, blank=True)
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "orders"
