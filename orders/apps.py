0b9e9dc0775e79c30eddfd25a068734013142e31
Mike Mupararano <kudath@yahoo.co.uk>
2025-12-06 20:27:41 +0000

    Associated each Stripe payment with a unique identifier.

--- orders/apps.py
+++ orders/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
+
+class Order(models.Model):
+    stripe_id = models.name = models.CharField(max_length=250, blank=True)
