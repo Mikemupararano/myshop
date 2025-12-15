8704470ead7ca17ae9f9f0e948fabb6b9f087208
Mike Mupararano <kudath@yahoo.co.uk>
2025-12-15 21:12:28 +0000

    Built the coupon model and made migrations.

--- coupons/models.py
+++ coupons/models.py
@@ -1,3 +1,19 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        help_text="Percentage value (0 and 100)"
+    )
+    active = models.BooleanField()
+    
+
+    def __str__(self):
+        return self.code
+    
+        
