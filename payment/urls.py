147bde01a154dfb4812f1bdf5739b44c955e344d
Mike Mupararano <kudath@yahoo.co.uk>
2025-12-15 10:41:40 +0000

    Replaced the project urls.py because there was a typo (payments) instead of "payment".

--- payment/urls.py
+++ payment/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("process/", views.payment_process, name="process"),
     path("completed/", views.payment_completed, name="completed"),
     path("cancelled/", views.payment_cancelled, name="cancelled"),
-    path("webhook/", webhooks.stripe_webhook, name="stripe-webhook"),  # ✅ FIX
+    path("webhook/", webhooks.stripe_webhook, name="stripe-webhook"),  
 ]
