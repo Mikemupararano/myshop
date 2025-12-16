8f38fbb0e5b780f5f6bb67e97b1426199f96dd3c
Mike Mupararano <kudath@yahoo.co.uk>
2025-12-16 14:49:40 +0000

    The Cart class is now prepared to handle a coupon applied to the current session and apply the corresponding discount.

--- cart/cart.py
+++ cart/cart.py
@@ -78,3 +78,22 @@ class Cart:
         return sum(
             Decimal(item["price"]) * item["quantity"] for item in self.cart.values()
         )
+        
+     @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (
+                self.coupon.discount / Decimal(100)
+            ) * self.get_total_price()
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
