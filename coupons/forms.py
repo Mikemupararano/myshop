89b0d336f90d1f8c4d242a4c9b851109cbbf42fd
Mike Mupararano <kudath@yahoo.co.uk>
2025-12-20 18:29:00 +0000

    Added label attribute to the quantity field of the CartAddProductForm.

--- coupons/forms.py
+++ coupons/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 
 class CouponApplyForm(forms.Form):
     code = forms.CharField()
+
+    PRODUCT_QUANTITY_CHOICES = [(i, _(str(i))) for i in range(1, 21)]
+
+    class CartAddProductForm(forms.Form):
+        quantity = forms.TypedChoiceField(
+            choices=PRODUCT_QUANTITY_CHOICES,
+            coerce=int,
+            label=_("Quantity"),
+        )
+        override = forms.BooleanField(
+            required=False,
+            initial=False,
+            widget=forms.HiddenInput,
+        )
--- orders/models.py
+++ orders/models.py
@@ -9,7 +9,7 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class Order(models.Model):
     first_name = models.CharField(_("first name"), max_length=50)
-    last_name = models.CharField(max_length=50)
+    last_name = models.CharField(_("last name"), max_length=50)
     email = models.EmailField(_("email"))
     address = models.CharField(_("address"), max_length=250)
     postal_code = models.CharField(_("postal code"), max_length=20)
