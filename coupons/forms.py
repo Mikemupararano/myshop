fcb5d85e512355fdd490a72a705ebb51e75c65cb
Mike Mupararano <kudath@yahoo.co.uk>
2025-12-20 19:54:52 +0000

    Added a label to the code field and marked it for translation.

--- coupons/forms.py
+++ coupons/forms.py
@@ -3,18 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CouponApplyForm(forms.Form):
-    code = forms.CharField()
+    code = forms.CharField(label=_("Coupon"))
 
-    PRODUCT_QUANTITY_CHOICES = [(i, _(str(i))) for i in range(1, 21)]
-
-    class CartAddProductForm(forms.Form):
-        quantity = forms.TypedChoiceField(
-            choices=PRODUCT_QUANTITY_CHOICES,
-            coerce=int,
-            label=_("Quantity"),
-        )
-        override = forms.BooleanField(
-            required=False,
-            initial=False,
-            widget=forms.HiddenInput,
-        )
+    
