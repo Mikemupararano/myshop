43b8730ac0b798c1cfce94f9159fe451d61b57cc
Mike Mupararano <kudath@yahoo.co.uk>
2025-12-21 19:53:56 +0000

    Added language prefix to URLs.

--- myshop/urls.py
+++ myshop/urls.py
@@ -6,17 +6,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
     path("cart/", include(("cart.urls", "cart"), namespace="cart")),
     path("orders/", include(("orders.urls", "orders"), namespace="orders")),
     # ✅ FIX: namespace must match payment/urls.py -> app_name = "payment"
     path("payment/", include(("payment.urls", "payment"), namespace="payment")),
     path("coupons/", include(("coupons.urls", "coupons"), namespace="coupons")),
-    path('rosetta/', include('rosetta.urls')),
+    path("rosetta/", include("rosetta.urls")),
     path("", include(("shop.urls", "shop"), namespace="shop")),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
