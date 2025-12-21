0e78cb3b8bcfcd49a745a41fda63fdaf3f7a8084
Mike Mupararano <kudath@yahoo.co.uk>
2025-12-21 15:49:20 +0000

    Installed django-rosetta.

--- myshop/settings.py
+++ myshop/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "payment.apps.PaymentConfig",
     "shop.apps.ShopConfig",
     "coupons.apps.CouponsConfig",
+    "rosetta",
 ]
 
 # -------------------------------------------------------------------
