35068198f59f2499436552177d1755691dfd8def
Mike Mupararano <kudath@yahoo.co.uk>
2025-12-21 20:37:22 +0000

    Removed url for webhooks from payment/urls.py and included it in the main urls.py. Webhook url has been added outside of i18n_patterns() to ensure we do not maintain a single URL for STRIPE event notifications.

--- myshop/urls.py
+++ myshop/urls.py
@@ -7,17 +7,24 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
+from payment import webhooks
 
 urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
-    path("cart/", include(("cart.urls", "cart"), namespace="cart")),
-    path("orders/", include(("orders.urls", "orders"), namespace="orders")),
+    path(_("cart/"), include(("cart.urls", "cart"), namespace="cart")),
+    path(_("orders/"), include(("orders.urls", "orders"), namespace="orders")),
     # ✅ FIX: namespace must match payment/urls.py -> app_name = "payment"
-    path("payment/", include(("payment.urls", "payment"), namespace="payment")),
-    path("coupons/", include(("coupons.urls", "coupons"), namespace="coupons")),
-    path("rosetta/", include("rosetta.urls")),
+    path(_("payment/"), include(("payment.urls", "payment"), namespace="payment")),
+    path(_("coupons/"), include(("coupons.urls", "coupons"), namespace="coupons")),
+    path(_("rosetta/"), include("rosetta.urls")),
     path("", include(("shop.urls", "shop"), namespace="shop")),
 )
+
+urlpatterns += [
+    path("payment/webhook/", webhooks.stripe_webhook, name="payment-webhook"),
+]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
