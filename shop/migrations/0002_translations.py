e965ef30c65fc1132df85d46e1945df85e5c6940
Mike Mupararano <kudath@yahoo.co.uk>
2025-12-22 20:39:50 +0000

    Models now synchronised with database.

--- shop/migrations/0002_translations.py
+++ shop/migrations/0002_translations.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('shop', '0001_initial'),
+        ("shop", "0001_initial"),
     ]
 
     operations = [
